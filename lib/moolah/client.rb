0672fcd2e6e361ebaff369856fef641f4d7e2df4
Daejuan Jacobs <djacobs@cloudrck.com>
2014-04-18 21:33:00 -0500

    Remove Unimplemeted Functions

--- lib/moolah/client.rb
+++ lib/moolah/client.rb
@@ -9,20 +9,6 @@ module Moolah
   class Client
     include HTTParty
     base_uri 'https://moolah.io'
-
-    def balance options={}
-      h = get '/account/balance', options
-      h['amount'].to_money(h['currency'])
-    end
-
-    def receive_address options={}
-      get '/account/receive_address', options
-    end
-    
-    def generate_receive_address (options={})
-      post '/api/pay', options
-    end
-
     # Get URL
     def generate_url_payment (options={})
     	r = self.class.get('/api/pay', options)

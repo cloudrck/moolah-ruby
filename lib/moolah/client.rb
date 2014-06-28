431a25ad53786242add29868417d006dc04672c2
Daejuan Jacobs <djacobs@cloudrck.com>
2014-06-28 11:08:32 -0500

    Add Headers

--- lib/moolah/client.rb
+++ lib/moolah/client.rb
@@ -9,9 +9,13 @@ module Moolah
   class Client
     include HTTParty
     base_uri 'https://moolah.io'
+    
+    def initialize
+		  @headers = {headers: {'User-Agent' => 'application/ruby.sinatra+json'} }}
+	  end
     # Get URL
     def generate_url_payment (options={})
-    	r = self.class.get('/api/pay', options)
+    	r = self.class.get('/api/pay', :query => options, :headers => @headers)
     	return r
     end
   end

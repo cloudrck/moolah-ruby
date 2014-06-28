169964473bd44dccade0f9a01e8d3dc2f137d081
Daejuan Jacobs <djacobs@cloudrck.com>
2014-06-28 11:09:09 -0500

    Update client.rb

--- lib/moolah/client.rb
+++ lib/moolah/client.rb
@@ -1,9 +1,5 @@
 require 'httparty'
-require 'multi_json'
-require 'hashie'
-require 'money'
-require 'time'
-require 'securerandom'
+
 
 module Moolah
   class Client
@@ -11,8 +7,8 @@ module Moolah
     base_uri 'https://moolah.io'
     
     def initialize
-		  @headers = {headers: {'User-Agent' => 'application/ruby.sinatra+json'} }}
-	  end
+		@headers = {headers: {'User-Agent' => 'application/ruby.sinatra+json'} }}
+	end
     # Get URL
     def generate_url_payment (options={})
     	r = self.class.get('/api/pay', :query => options, :headers => @headers)
